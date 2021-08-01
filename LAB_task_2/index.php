63638bd6510fbfcbdea0a80fece15fb9459c90d0
Sojeeb_Roy <sojeebroy217@gmail.com>
2021-08-01 16:32:10 +0600

    error fixed

--- LAB_task_2/index.php
+++ LAB_task_2/index.php
@@ -1,13 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head></head>
-	<?php $name="Sojeeb";?>
-<body>
-	<h1 align="center">My Profile</h1>
-	<table align="center">
-		<tr>
-			<td><?php echo $name;?></td>
-		</tr>
-	</table>
-</body>
-</html>
+
+<script>
+	function get(id)
+	{
+		return document.getElementById(id);
+	}
+
+	function loadDoc()
+	{
+		var xhr=new XMLHttpRequest();
+		xhr.open("GET","MyProfile.php",true);
+		xhr.onreadstatechange=function(){
+			if(this.readState==4 && this.status==200){
+				get("demo").innerHTML=this.responseText;
+			}
+		};
+		xhr.send();
+	}
+</script>
+<button onclick="loadDoc()">ClickMe</button>
+<div id="demo"></div>
