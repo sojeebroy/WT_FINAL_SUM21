7139cf223a476b83635ab7f96a1ded0e5efd8a57
Sojeeb_Roy <sojeebroy217@gmail.com>
2021-08-01 15:32:18 +0600

    lab task 2

---
+++ LAB_task_2/MyProfile.php
@@ -0,0 +1,20 @@
+<script>
+	function get(id)
+	{
+		return document.getElementById(id);
+	}
+
+	function loadDoc()
+	{
+		var xhr=new XMLHttpRequest();
+		xhr.open("GET","MyProfile.php",true);
+		xhr.onreadstatechange=function(){
+			if(this.readState==4 && this.status==200){
+				get("demo").innerHTML=this.responseText;
+			}
+		};
+		xhr.send();
+	}
+</script>
+<button onclick="loadDoc()">ClickMe</button>
+<div id="demo"></div>
---
+++ LAB_task_2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+	<?php $name="Sojeeb";?>
+<body>
+	<h1 align="center">My Profile</h1>
+	<table align="center">
+		<tr>
+			<td><?php echo $name;?></td>
+		</tr>
+	</table>
+</body>
+</html>
